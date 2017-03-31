55d159d0047623db2c284f7506697f7b7f3de1d7
midhunks <mr.midhunks@gmail.com>
2017-03-30 23:41:55 -0400

    NA

--- Matlab Files/Backup/State_transition.m
+++
@@ -1,9 +0,0 @@
-function S_transition = State_transition(S,Stoichiometry)
-global number_reactions number_species nstates
-S_transition = zeros(nstates,number_species,number_reactions);
-for i=1:number_reactions
-    for j= 1: number_species
-        S_transition(:,j,i) = S(:,j) - Stoichiometry (j,i);
-    end
-end
-end
