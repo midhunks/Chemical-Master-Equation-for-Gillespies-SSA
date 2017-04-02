22707431f22cc46db0b6cd93e355bda874f9ab96
midhunks <mr.midhunks@gmail.com>
2017-04-02 01:12:24 -0400

    Main file

--- Main.m
+++ Main.m
@@ -1,7 +1,9 @@
-%% Step 0: Clearing variables and setting defaults
+%% Step 0: Clearing variables and setting defaults for plots
+
 Start_UP
 
 %% Step 1: Add subdirectories to the search path
+
 if ispc, b='\'; else, b='/'; end % defining forward/bckward slashes
 addpath([pwd,b,'Generalized_Functions',b]);
 addpath([pwd,b,'Models',b]);
@@ -11,10 +13,14 @@ addpath([pwd,b,'CME',b,'State_Space_Builder',b]);
 addpath([pwd,b,'CME',b,'Dynamics_Builder',b]);
 
 %% Step 2: Generate the biochemical model
-MM % File which describe the Biochemical model
+% File which describe the Biochemical model
+
+MM
 
 %% Step 3: Generate the CME
+
 CME
 
 %% Step 4: Generate sample paths via SSA
+
 SSA
